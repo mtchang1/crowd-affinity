3b9b66330044736ffe9680b38ba2339785a8fe33
Mark Whiting <mark.whiting@gmail.com>
2014-05-05 13:43:49 -0400

    Added a tree view to data analysis
    Now prints a tree of questions with question children and answers on
    each layer.
    
    We currently also include ratings.

--- reading data.py
+++ reading data.py
@@ -1,8 +1,9 @@
+import pprint
 import sqlite3
 conn = sqlite3.connect('database test 1.db')
 c = conn.cursor()
 
-#question tree structure
+
 #compare starting point to ending point
 #how true are the ratings compared with expert ratings
 #what questions had the best results
@@ -11,6 +12,16 @@ c = conn.cursor()
 # 	c.execute('SELECT * FROM crowd_affinity_answer WHERE question_id =?', i)
 # 	print c.fetchone()
 
-for row in c.execute('SELECT parent_id,question_text FROM crowd_affinity_question ORDER BY parent_id'):
-	print row
-        # print c.execute('SELECT parent_id FROM crowd_affinity_question ORDER BY id'):
+answers = []
+for answer in c.execute('SELECT * FROM crowd_affinity_answer ORDER BY question_id'):
+	answers.append(answer)
+
+questionTree = {}
+for question in c.execute('SELECT * FROM crowd_affinity_question ORDER BY parent_id'):
+	if int(question[2]) == 0: 
+		questionTree[question[0]] = {'Question':question[1],'Answers':[[answer[0], answer[4]/(answer[5]+.0001), answer[2]] for answer in answers if answer[1]== question[0]]}
+	if question[3] in questionTree:
+		questionTree[question[3]][question[0]] = {'Question':question[1], 'Answers':[[answer[0], answer[4]/(answer[5]+.0001), answer[2]] for answer in answers if answer[1]== question[0]]}
+
+#question tree structure
+pprint.pprint(questionTree, depth=10)
